22c413f5086d451aab8825adf843def2765a7ceb
Domenica-2003 <domenicabaerwald771@gmail.com>
2023-10-17 13:52:00 -0700

    Wk6 Updates

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,10 +1,10 @@
-let toggleMenu = _ => {
-    const menuButton = document.getElementById('menu-button');
-    const menuItems = document.getElementsByClassName('menu-items');
+const menuButton = document.getElementById('menu-button');
+const menuItems = document.getElementById('menu-items');
 
-
+menuButton.addEventListener('click', () => {
     menuItems.classList.toggle('opened');
-    menuButton.classList.toggle('opened');
-
-
-}
+    if (menuButton.textContent === 'MENU')
+        menuButton.textContent = 'X';
+    else 
+        menuButton.textContent = 'MENU'
+})
